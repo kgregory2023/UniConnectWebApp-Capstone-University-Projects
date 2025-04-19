842b8a7343568eb1a4439af72efc51e124f37545
MMcClure313 <mpm35@students.uwf.edu>
2025-04-19 12:42:37 -0500

    Update connect.jsx page

--- UnIFront/src/pages/connect/Connect.jsx
+++ UnIFront/src/pages/connect/Connect.jsx
@@ -98,7 +98,12 @@ function Connect() {
         const swipedUser = cards.find(card => card.id === id);//Grabs the entire card data for the contact
 
         if (direction === "right" && swipedUser) {
-            setLikedUsers(prev => [...prev, swipedUser]);
+
+            const alreadyLiked = likedUsers.some(user => user.id == swipedUser.id);
+
+            if(!alreadyLiked){ //Simple fix for the connect page. All it does is not add the user if they're already liked.
+                setLikedUsers(prev => [...prev, swipedUser]);
+            }
         }
 
         setCards(cards.filter(card => card.id !== id));
@@ -215,71 +220,3 @@ function Connect() {
 }
 
 export default Connect;
-
-
-/**OLD
- * Initial page for connect, card swipe feature. Commented out is a series of 3 initial cards for testing purposes.
- * 
- * @author @MMcClure313
- 
-
-import {useState, React } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { useUser } from '../../components/userContext/UserContext';
-import CardSwipe from '../../components/cardSwipe/cardSwipe';
-import './Connect.css';
-
-function Connect() {
-    const navigate = useNavigate();
-    const { user, token } = useUser();
-
-    //Say this is where a function would live that would fetch users (30-50) from the database, sort and algorithmically serve the user 5-10 based on matching tags
-    //That is why user and token are imported, as not now but later, we will have to check user tags against other user tags.
-
-    const initialCards = [
-        { id: 1, text: "Card 1" },
-        { id: 2, text: "Card 2" },
-        { id: 3, text: "Card 3" },
-    ];
-
-    const CardStack = () => {
-        const [cards, setCards] = useState(initialCards);
-
-        const handleSwipe = (direction, id) => {
-            console.log(`Swiped ${direction} on card ${id}`);
-            setCards(cards.filter(card => card.id !== id)); //I love JS and their array functions 😍
-        }
-
-        return (
-                <div className="card-stack">
-                    {cards.length > 0 ? (
-                        cards.map((card, index) => <CardSwipe
-                         key={card.id}
-                         card={card}
-                         onSwipe={handleSwipe} 
-                         style={{
-                            position: 'absolute',
-                            zIndex: cards.length - index,  // Ensure they stack in order (front-to-back)
-                            border: '1px solid black',
-                         }}
-                         />)
-
-                    ) : (
-                        <p>No more!!!</p>
-                    )}
-                </div>
-        );
-    } // This is the end of the CARDSTACK return.
-
-    return (
-        <div className="connect">
-            <h1 className="padding-70">Connect</h1>
-            <CardStack/>
-        </div>
-    )
-}
-
-export default Connect;
-
-*/
