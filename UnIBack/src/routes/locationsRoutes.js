73f12af44872fe2e4632e23d7253dc54aedfeaa4
kgregory2023 <kpg11@students.uwf.edu>
2025-04-21 02:43:33 -0500

    Location Routes Middleware Implementation
    Implemented Location RoleBased admin control

--- UnIBack/src/routes/locationsRoutes.js
+++ UnIBack/src/routes/locationsRoutes.js
@@ -1,14 +1,20 @@
 const express = require("express")
 const ratingRoutes = require("../routes/ratingsRoutes");
+const authorizeRoles = require("../roleMiddleware");
 const { createLocation, getLocationById, getAllLocations, updateLocation, deleteLocation } = require("../controllers/locationController");
 const locationRoutes = express.Router();
 
-locationRoutes.post("/", createLocation);
 locationRoutes.get("/:id", getLocationById);
 locationRoutes.get("/", getAllLocations);
 locationRoutes.put("/:id", updateLocation);
 locationRoutes.delete("/:id", deleteLocation);
 
+locationRoutes.post(
+    "/create",
+    authenticateToken,
+    authorizeRoles(["admin"]),
+    createLocation
+);
 locationRoutes.use('/:locationId/ratings', ratingRoutes);
 
 module.exports = locationRoutes;
